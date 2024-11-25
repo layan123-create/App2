a750503865ee0769d4bcd74dd7a6c5ea8e769be9
layan123-create <layanawad123123@gmail.com>
2024-11-25 18:59:47 +0200

    first commit

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +13,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etName,etYear;
+    private TextView tvShowDetails;
+    private String name,details;
+    private int year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etName=findViewById(R.id.etName);
+        etYear=findViewById(R.id.etYear);
+        tvShowDetails=findViewById(R.id.tvShowDetails);
+
     }
+    public void shoDetails(View view){
+        if(etName.getText().toString().isEmpty() || etYear.getText().toString().isEmpty()){
+            Toast.makeText(this,"Please fill in fields..",Toast.LENGTH_LONG).show();
+        }
+        else {
+            name=etName.getText().toString();
+            year=Integer.parseInt(etYear.getText().toString());
+            int age=2024-year;
+            details="Welcome "+name+"\nYour age is "+age+" years";
+            tvShowDetails.setText(details);
+        }
+    }
+
 }
